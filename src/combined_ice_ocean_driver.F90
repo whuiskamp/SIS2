8d421e1eb76128a20fcb63181a1fc273f907b8a9
Willem Huiskamp <willemhuiskamp88@gmail.com>
2022-06-29 15:01:35 +0200

    Added more log statements

--- src/combined_ice_ocean_driver.F90
+++ src/combined_ice_ocean_driver.F90
@@ -314,6 +314,9 @@ subroutine direct_flux_ice_to_IOB(Time, Ice, IOB, do_thermo)
     call data_override('OCN', 'basal_hflx', IOB%basal_hflx, Time)
     call MOM_error(WARNING, "ice_ocean_driver_init successfully "// &
                     "read in fields for basal melt and heat flux")
+  else
+    call MOM_error(WARNING, "ice_ocean_driver_init has not "// &
+                    "read in fields for basal melt and heat flux")
   endif
   call data_override('OCN', 'p',         IOB%p        , Time)
   call data_override('OCN', 'mi',        IOB%mi       , Time)
